87d92b6498b409c8e955d9533a2f81d5e5e1128e
abullahi <49534483+huah1600@users.noreply.github.com>
2023-02-13 18:51:13 +0100

    Har skrivit ett script

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(
+                "Welcome to Softhunt.net");
+    }
+}
+
+// Outputs "I just got executed!"
