9b03f8d671774a24429fc0354198f4051af52882
abullahi <49534483+huah1600@users.noreply.github.com>
2023-02-13 19:18:59 +0100

    Har ändra på scriptet

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(
-                "Welcome to Softhunt.net");
+        System.out.println("Welcome to Softhunt.net");
+        System.out.println("hej")
+                
     }
 }
 
