534991def18dfb8d2c20c6faf014c0d95dcfed44
abullahi <49534483+huah1600@users.noreply.github.com>
2023-02-13 19:32:47 +0100

    done

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Softhunt.net");
-        System.out.println("hej")
+        System.out.println("hej"); 
+        System.out.println("hejdå"); 
                 
     }
 }
