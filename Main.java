97bcd8149b88964139ac9ac5671d79a1f6159b9f
abullahi <49534483+huah1600@users.noreply.github.com>
2023-02-27 17:39:39 +0100

    jag har lagt till ett h

--- Main.java
+++ Main.java
@@ -2,9 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Softhunt.net");
-        System.out.println("hej"); 
-        System.out.println("hejdå"); 
-                
+        System.out.println("hej");
+        System.out.println("hejdå");
+        System.out.println("H");
+
     }
 }
 
